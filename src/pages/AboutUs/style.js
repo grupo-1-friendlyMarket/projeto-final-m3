3c7a0372239b2d45dddf46b13d57a88826481585
Nathalia Pinheiro <nathaliapinheiro3@outlook.com>
2022-07-14 17:40:04 -0300

    fix: remove {}

--- src/pages/AboutUs/style.js
+++ src/pages/AboutUs/style.js
@@ -27,13 +27,12 @@ export const Container = styled.div`
     }
   }
 
-    h1{
-        font-size: 22px;
-    }
+  h1 {
+    font-size: 22px;
+  }
 
-    .imgAbout {
-      width: 42rem;
-    }
+  .imgAbout {
+    width: 42rem;
   }
 
   .right {
@@ -76,74 +75,72 @@ export const Container = styled.div`
     width: 36vw;
     display: flex;
 
-      img {
-        width: 120px;
-        height: 120px;
-        border-radius: 100%;
-        margin-left: 15px;
-      }
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 100%;
+      margin-left: 15px;
     }
   }
 
-    hr {
-      display: none;
+  hr {
+    display: none;
+  }
+
+  .textContainer {
+    width: 65%;
+    margin-left: 18px;
+    margin-top: 4px;
+
+    h3 {
+      font-size: 20px;
     }
 
-    .textContainer {
-      width: 65%;
-      margin-left: 18px;
-      margin-top: 4px;
-
-      h3 {
-        font-size: 20px;
-      }
-
-      p{
-        font-size: 15px;
-        margin-top: 7px;
-        line-height: 1.2;
-      }
+    p {
+      font-size: 15px;
+      margin-top: 7px;
+      line-height: 1.2;
     }
   }
 
-    @media screen and (min-width: 319px) and (max-width: 1300px){
-      .aboutusContainer{
-        flex-direction: column;
-        width: 87vw;
-        gap: 1rem;
-      }
-
-      .left{
-        width: auto;
-        text-align: center;
-        /* margin-top: 2rem; */
-      }
-
-      .imgAbout{
-        width: 22rem !important;   
-      }
-
-      .right{
-        width: auto;
-        margin: 0;
-        font-weight: 200;
-      }
-
-      .cardContainer{
-        flex-direction: column;
-        margin: 0;
-        width: auto;
-      }
-
-      .apresentation{
-        width: 85vw;
-        border: none;
-      }
-
-      .card{
-        width: auto;
-        height: auto;
-        margin: 1rem 0;
-      }
+  @media screen and (min-width: 319px) and (max-width: 1300px) {
+    .aboutusContainer {
+      flex-direction: column;
+      width: 87vw;
+      gap: 1rem;
     }
+
+    .left {
+      width: auto;
+      text-align: center;
+      /* margin-top: 2rem; */
+    }
+
+    .imgAbout {
+      width: 22rem !important;
+    }
+
+    .right {
+      width: auto;
+      margin: 0;
+      font-weight: 200;
+    }
+
+    .cardContainer {
+      flex-direction: column;
+      margin: 0;
+      width: auto;
+    }
+
+    .apresentation {
+      width: 85vw;
+      border: none;
+    }
+
+    .card {
+      width: auto;
+      height: auto;
+      margin: 1rem 0;
+    }
+  }
 `;
